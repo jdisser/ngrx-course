032890f0e957270f35d084c43e070e0086538085
jrdisser <jimdisser@altocirrusapps.com>
2019-07-20 15:02:32 -0600

    refactored logged in observables to use selectors to improve eff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import {Observable} from "rxjs";
 import {map} from 'rxjs/operators';
 import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from '@angular/router';
 import {AppState} from './reducers';
+import {isLoggedIn, isLoggedOut} from './auth/auth.selectors';
 
 
 @Component({
@@ -46,12 +47,12 @@ export class AppComponent implements OnInit {
 
       this.isLoggedIn$ = this.store
         .pipe(
-          map(state => !!state["auth"].user)
+          select(isLoggedIn)
         );
 
       this.isLoggedOut$ = this.store
         .pipe(
-          map(state => !state["auth"].user)
+          select(isLoggedOut)
         );
     }
 
